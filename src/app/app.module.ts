fbf383d3162f3d00603a427aaefeabd39d224e42
José Javier <jotatributo4@gmail.com>
2020-06-01 13:53:46 +0200

    añado proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from "@angular/core/";
+import {BrowserModule} from '@angular/platform-browser';
+import { AplicacionComponent} from './componentes/aplicacion.component';
+import { CrearUsuarioComponent } from './componentes/crear-usuario/crear-usuario.component';
+import { FormsModule} from '@angular/forms';
+import { ListaUsuariosComponent } from './componentes/lista-usuarios/lista-usuarios.component';
+import { EditarUsuarioComponent } from './componentes/editar-usuario/editar-usuario.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ErrorComponent } from './componentes/error/error.component';
 
-import { AppComponent } from './app.component';
-
+const rutasApp :Routes =[
+    {path:"menu",
+    component:MenuComponent},
+    {path:"crear",
+    component:CrearUsuarioComponent},
+    {path:"lista",
+    component:ListaUsuariosComponent},
+    {path:"",
+    redirectTo:"/menu",
+    pathMatch:"full"},
+    {path:"**",
+    component:ErrorComponent}
+    
+]
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations:[
+        AplicacionComponent,
+        CrearUsuarioComponent,
+        ListaUsuariosComponent,
+        EditarUsuarioComponent,
+        MenuComponent,
+        ErrorComponent
+    ],
+imports: [BrowserModule,FormsModule,
+    RouterModule.forRoot(rutasApp,{enableTracing:true})],
+bootstrap:[AplicacionComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+        //este módulo puede ser una clase vacía
+}
+
